6c68c358f616e5d9a5c50bacd8706b0ac4f26a19
Kirill Ryabov <sailor.kirill@gmail.com>
2024-02-01 16:07:35 +0700

    MSP-1207 Add param 'sep' to functions

--- flatten.go
+++ flatten.go
@@ -5,17 +5,17 @@ import (
 	"reflect"
 )
 
-func Flatten(value interface{}) map[string]interface{} {
-	return FlattenPrefixed(value, "")
+func Flatten(value interface{}, sep string) map[string]interface{} {
+	return FlattenPrefixed(value, sep, "")
 }
 
-func FlattenPrefixed(value interface{}, prefix string) map[string]interface{} {
+func FlattenPrefixed(value interface{}, sep string, prefix string) map[string]interface{} {
 	m := make(map[string]interface{}, 5)
-	FlattenPrefixedToResult(value, prefix, m)
+	FlattenPrefixedToResult(value, sep, prefix, m)
 	return m
 }
 
-func FlattenPrefixedToResult(value interface{}, prefix string, m map[string]interface{}) {
+func FlattenPrefixedToResult(value interface{}, sep string, prefix string, m map[string]interface{}) {
 	original := reflect.ValueOf(value)
 	kind := original.Kind()
 	if kind == reflect.Ptr || kind == reflect.Interface {
@@ -40,11 +40,11 @@ func FlattenPrefixedToResult(value interface{}, prefix string, m map[string]inte
 		keys := original.MapKeys()
 		base := ""
 		if prefix != "" {
-			base = prefix + "."
+			base = prefix + sep
 		}
 		for _, childKey := range keys {
 			childValue := original.MapIndex(childKey)
-			FlattenPrefixedToResult(childValue.Interface(), base+childKey.String(), m)
+			FlattenPrefixedToResult(childValue.Interface(), sep, base+childKey.String(), m)
 		}
 	case reflect.Struct:
 		numField := original.NumField()
@@ -57,16 +57,16 @@ func FlattenPrefixedToResult(value interface{}, prefix string, m map[string]inte
 				childKey = ""
 				base = prefix
 			} else if prefix != "" {
-				base = prefix + "."
+				base = prefix + sep
 			}
-			FlattenPrefixedToResult(childValue.Interface(), base+childKey, m)
+			FlattenPrefixedToResult(childValue.Interface(), sep, base+childKey, m)
 		}
 	case reflect.Array, reflect.Slice:
 		l := original.Len()
 		base := prefix
 		for i := 0; i < l; i++ {
 			childValue := original.Index(i)
-			FlattenPrefixedToResult(childValue.Interface(), fmt.Sprintf("%s.[%d]", base, i), m)
+			FlattenPrefixedToResult(childValue.Interface(), sep, fmt.Sprintf("%s%s[%d]", base, sep, i), m)
 		}
 	default:
 		if prefix != "" {
